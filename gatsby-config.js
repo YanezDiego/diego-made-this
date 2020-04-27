83bbc5e2ab79dfff32068bfbda73179eacc0c0fe
Diego Yanez <diegosyanez92@gmail.com>
2020-04-27 18:38:29 -0400

    change to use dy.png

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `./images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `./images/DY.png`, // This path is relative to the root of the site.
       },
     },
     {
