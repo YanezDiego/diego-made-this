f5d67746d934848b185ba14239925b06b5520a18
Diego Yanez <diegosyanez92@gmail.com>
2020-04-27 15:35:26 -0400

    import intro to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,22 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
+// import {css} from '@emotion/core'
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
+import Intro from './intro'
 import SEO from "../components/seo"
 
+
+
+
 const IndexPage = () => (
+
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Intro/>
   </Layout>
+
 )
 
 export default IndexPage
