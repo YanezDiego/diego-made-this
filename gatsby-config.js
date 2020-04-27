83fa5103f12ee864ba94c5d4b74f999c685674d1
Diego Yanez <diegosyanez92@gmail.com>
2020-04-26 21:18:04 -0400

    update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Diego Made This`,
+    description: `Diego Portfolio Page`,
+    author: `Diego Yanez`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
