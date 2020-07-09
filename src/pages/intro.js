581e218832766d1acfe52647d0bc8b7574bc00e5
Diego Yanez <diegosyanez92@gmail.com>
2020-07-09 19:47:06 -0400

    refactor

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -1,58 +1,67 @@
-import React  from 'react'
-import { css } from '@emotion/core'
-import Fade from 'react-reveal/Fade'
-import { Button, Container, Grid } from '@material-ui/core'
+import React from "react"
+import { css } from "@emotion/core"
+import Fade from "react-reveal/Fade"
+import { Button, Container, Grid } from "@material-ui/core"
 import Contact from "./contact"
 
-import {Link} from 'gatsby'
+import { Link } from "gatsby"
 
 const Intro = () => (
-        
-       <>
-           <Container maxWidth="sm">
-            {/* <Grid container xs={12} m={6} align="center" spacing={1} className="intro"> */}
-            <Grid item xs={12}>
-             <div className="text" css={css`
-                padding: 25%  0.5rem 0.04rem;
-                maxWidth: 100%
-             `}>
-            <Fade>
-               
-                <h1 css={css`
-                font-family: 
-                'Merriweather', serif,
-                'Open Sans', sans - serif,'Oswald', 
-                sans - serif,'Pacifico', 
-                cursive,'Josefin Slab', serif, 
-                'Francois One', sans - serif;
-                `}>Welcome, My Name is Diego</h1>
-                <p><span role="img">🗽</span> NYC is my home.</p>
-                <p><span role="img">👨‍💻</span> <b>Coding</b> feels like living in NYC.</p>
+  <>
+    {/* <Container maxWidth="sm"> */}
+    {/* <Grid container
+                spacing={6}
+                alignContent="center"
+                direction="column"
+                justify="center"
+                alignItems="flex-end"
                 
-            </Fade>
-            </div>
-            <div css={css`
-                   padding: 4px 10.25rem .5rem 10.25rem;
-                    width: 100%;
-            `}>
-            <Button variant='contained' orientation="center" color="primary" >
-            <Link to='/project' 
-                css={css`
-                    text-decoration: none;
-                    color:white;
-                `}>Projects</Link></Button>
-            </div>
-            </Grid>
-            
+            > */}
+    {/* <Grid item xs sm md lg xl> */}
+    <div
+      className="container"
+      //  css={css`
+      //     padding: 25%  0.5rem 0.04rem;
+      //     maxWidth: 100%
+      //  `}
+    >
+      <Fade>
+        <h1
+          css={css`
+            font-family: "Merriweather", serif, "Open Sans", sans - serif,
+              "Oswald", sans - serif, "Pacifico", cursive, "Josefin Slab", serif,
+              "Francois One", sans - serif;
+          `}
+        >
+          Welcome, My Name is Diego
+        </h1>
+        <p>
+          <span role="img">🗽</span> NYC is my home.
+        </p>
+        <p>
+          <span role="img">👨‍💻</span> <b>Coding</b> feels like living in NYC.
+        </p>
 
-            <Contact/>
+        <Button variant="contained" orientation="center" color="primary">
+          <Link
+            to="/project"
+            css={css`
+              text-decoration: none;
+              color: white;
+            `}
+          >
+            Projects
+          </Link>
+        </Button>
+      </Fade>
+      <Contact />
+    </div>
 
-            {/* </Grid> */}
-            </Container>
-        </> 
-        
-    )
+    {/* </Grid> */}
 
-
+    {/* </Grid> */}
+    {/* </Container> */}
+  </>
+)
 
 export default Intro
